d0546ae08afb1bf28193342023dc8cb5a204871b
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2024-01-02 19:43:19 +0000

    feat: Added createExportKey() internal utility.
    feat: Added createExportKey() internal utility.

---
+++ src/internals/utils.ts
@@ -0,0 +1,40 @@
+/*****************************************************************************************************************/
+
+// @author         Michael Roberts <michael@observerly.com>
+// @package        @observerly/nitro-csrf
+// @license        Copyright © 2021-2023 observerly
+
+/*****************************************************************************************************************/
+
+import { subtle } from 'uncrypto'
+
+/*****************************************************************************************************************/
+
+// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/encrypt#aes-cbc
+const defaultEncryptAlgorithm = {
+  name: 'AES-CBC',
+  length: 256
+} as const satisfies AesKeyAlgorithm
+
+/*****************************************************************************************************************/
+
+/**
+ *
+ * createExportKey
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/generateKey
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/exportKey
+ *
+ */
+export const createExportKey = async (params?: {
+  encryptAlgorithm?: AesKeyAlgorithm
+}): Promise<JsonWebKey> => {
+  const { encryptAlgorithm = defaultEncryptAlgorithm } = params || {}
+  // Generate a key that can be used with AES-CBC, for symmetric encryption and decryption.
+  const key = await subtle.generateKey(encryptAlgorithm, true, ['encrypt', 'decrypt'])
+  // Provide the key in extractable, JWK format:
+  return await subtle.exportKey('jwk', key)
+}
+
+/*****************************************************************************************************************/
+
